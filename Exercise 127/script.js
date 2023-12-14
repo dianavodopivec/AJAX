e15208b329c4b1eb970fa6931a8a4a9553ab5c01
Diana Vodopivec <dianavodopivec@MacBook-Air-de-Matias.local>
2023-12-14 15:43:15 -0300

    Ajax Exercise 127

--- Exercise 127/script.js
+++ Exercise 127/script.js
@@ -1,6 +1,35 @@
 const d = document;
-const $form = d.querySelector("form")
+const $form = d.querySelector("form");
+const $input = d.querySelectorAll(".contact-form[required]");
 
-const contactFormValidation = () => {
-    const $form = d.querySelector("form")
-}
+const formValidations = () => {
+  // PRINTER
+  $input.forEach((input) => {
+    const $span = d.createElement("span");
+    $span.id = input.name;
+    $span.textContent = input.title;
+    $span.classList.add("contact-form-error", "none");
+    input.insertAdjacentElement("afterend", $span);
+  });
+
+  // LISTENER
+  d.addEventListener("keyup", (e) => {
+    if (e.target.matches(".contact-form[required]")) {
+      const $input = e.target,
+        pattern = $input.pattern || $input.dataset.pattern;
+
+      if (pattern && $input.value !== "") {
+        let regex = new RegExp(pattern);
+        return !regex.exec($input.value)
+          ? d.getElementById($input.name).classList.add("is-active")
+          : d.getElementById($input.name).classList.remove("is-active");
+      } else {
+        return $input.value !== ""
+          ? d.getElementById($input.name).classList.add("is-active")
+          : d.getElementById($input.name).classList.remove("is-active");
+      }
+    }
+  });
+};
+
+formValidations();
