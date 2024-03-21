18f011f5da17a103389142a0e587d1970a883975
Diana <dianavodopivec16@gmail.com>
2024-03-21 16:20:03 -0300

    AJAX: Exercise 134/135

---
+++ Exercise 134/135/api-pokemon.js
@@ -0,0 +1,17 @@
+const $header = document.querySelector("header");
+const $main = document.querySelector("main");
+const $links = document.querySelector(".links");
+
+const pokeApi = "https://pokeapi.co/api/v2/pokemon/";
+
+const consumePokemonApi = async (url) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+
+document.addEventListener("DOMContentLoaded", e => {
+    consumePokemonApi(pokeApi)
+})
