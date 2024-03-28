3255ca69c2e8b07c3d0a946011643fcc7539784e
Diana <dianavodopivec16@gmail.com>
2024-03-28 11:12:59 -0300

    AJAX: Exercise 137

---
+++ Exercise 137/api-musica.js
@@ -0,0 +1,8 @@
+const $form = document.getElementById("buscador-musica");
+const $cargador = document.querySelector(".cargador");
+const $error = document.querySelector(".error");
+const $main = document.querySelector("main");
+const $artista = document.querySelector(".artista");
+const $cancion = document.querySelector(".cancion");
+
+//NO ANDA LA API//
